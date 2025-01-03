7d388b781a1fc52ce7858145805fdfc3c20b9793
nasr <naserallahataya@gmail.com>
2025-01-03 09:07:12 +0300

    create splash view body

---
+++ lib/features/presentaion/views/widget/splash_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashviewBody extends StatelessWidget {
+  const SplashviewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
