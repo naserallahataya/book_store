8647735ff475c8bc3fa812ddb75c3a211a2e1bc0
nasr <naserallahataya@gmail.com>
2025-01-04 05:22:57 +0300

    add feature list view to home

--- lib/features/home/preserntation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/preserntation/views/widgets/custom_list_view_item.dart
@@ -1,21 +1,18 @@
 import 'package:book_store/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                  fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+                fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
       ),
     );
   }
